82c0ef43ecdb9c14984b73d3e5c9eb47448b2654
Tim Wright <mithgirw@gmail.com>
2016-07-05 15:18:56 -0600

    final layout

--- app/app.js
+++ app/app.js
@@ -5,10 +5,11 @@ $(document).ready(function(){
     /** Get User input--Search with it*/
     $("#search-form").submit(function(event) {
         event.preventDefault();
-        $(".col-md-4").remove();
+        $(".col-md-12").remove();
         var searchVal = $("#search-input").val();
         getResults(searchVal);
         $("#search-input").val("");
+        $("#message").remove();
     });
 
     /** Get info from youtube */
@@ -30,15 +31,24 @@ $(document).ready(function(){
     function showResults(results){
         $.each(results, function(index, value){
             console.log(value.snippet.title);  /** title is a key in the object  */
-            $("#results-row").append();
-            $("#results-row").append("<div class='col-md-4'>" + 
-                "<h3>" + value.snippet.title + "</h3>" +
+            //$("#title-row").append(
+            $("#results-row").append(
+                //video title
+                "<div class='col-md-12 text-center'>" + "<h3>" + value.snippet.title + "</h3>" +
+                //video
+                "<div class='col-md-12'>" + 
+                //"<h3>" + value.snippet.title + "</h3>" +
                 "<span>" + 
-                "<a href=" + 'https://www.youtube.com/watch?v=' + value.id.videoId + ">" +
-                "<img src=" + value.snippet.thumbnails.medium.url + ">" + "</span>" + 
-                "</a>" +
+                    //"<iframe width='420' height='315' src=http://www.youtube.com/embed/" + value.id.videoId + "?autoplay=0></iframe>" +
+                    "<a href=" + 'https://www.youtube.com/watch?v=' + value.id.videoId + ">" +
+                        "<img src=" + value.snippet.thumbnails.medium.url + ">" +
+                    "</a>" +
+                "</span>" +
                 "</div>");
         });
+
+
+        
         
     }
 
